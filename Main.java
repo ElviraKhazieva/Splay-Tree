80df3587ddab3bc47fe6b309133a1cb915abc940
Elvira <hazim219@yandex.ru>
2020-05-20 23:04:14 +0300

    Информация о себе

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Хазиева Эльвира, 11-901");
+
+        System.out.println("дата рождения - 9 декабря 2001 года");
+
+
+    }
+}
